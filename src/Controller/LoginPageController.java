171967339da90be01904966a08798b12e774facc
cheehong146 <cheehong146@gmail.com>
2018-10-13 17:56:18 +0800

    added simulationPage

--- src/Controller/LoginPageController.java
+++ src/Controller/LoginPageController.java
@@ -7,6 +7,9 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginPageController extends Navigation {
 
     @FXML
@@ -23,7 +26,7 @@ public class LoginPageController extends Navigation {
     private PasswordField txtPassword;
 
     private String url = "";
-    private int points = 0;
+    private int totPoints;
 
     @FXML
     private void btnLoginClick(ActionEvent actionEvent) {
@@ -34,9 +37,53 @@ public class LoginPageController extends Navigation {
 
     @FXML
     private void btnPointClick(ActionEvent actionEvent) {
-        if (url.contains("https")) {
+        totPoints = calcPointByPassword(txtPassword.getText());
+        totPoints += calcPointByUrl(url);
+        System.out.println("Total Points: " + totPoints);
+    }
+
+    private int calcPointByUrl(String url){
+        int points = 0;
+        if(url.contains("https"))
+            points = 10;
+
+        return  points;
+    }
+
+    private int calcPointByPassword(String pass){
+        int points = 0;
+        boolean lowercaseFlag = false;
+        boolean uppercaseFlag = false;
+        boolean numberFlag = false;
+        String regexSymbol = "[:?!@#$%^&*()]";
+
+        if (pass.length() >= 8)//I Assume is 8 charac and above?
             points += 10;
+
+        char [] chPass = pass.toCharArray();
+        //check if lowercase, uppercase, or contain digit
+        for (char ch :
+                chPass) {
+            if (Character.isLowerCase(ch))
+                lowercaseFlag = true;
+            else if(Character.isUpperCase(ch))
+                uppercaseFlag = true;
+            else if(Character.isDigit(ch))
+                numberFlag = true;
+
+            if(lowercaseFlag && uppercaseFlag && numberFlag)
+                break;
         }
-        System.out.println("Points: " + points);
+        points += lowercaseFlag? 10: 0;
+        points += uppercaseFlag? 10: 0;
+        points += numberFlag? 10: 0;
+
+        //check if password contain symbol
+        Pattern pattern = Pattern.compile(regexSymbol);
+        Matcher matcher = pattern.matcher(pass);
+        if (matcher.find())
+            points += 10;
+
+        return points;
     }
 }
