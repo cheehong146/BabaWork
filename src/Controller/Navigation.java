b6f83604cd8ce19933a79c0134bd8ce4f1d6a09d
cheehong146 <cheehong146@gmail.com>
2018-10-13 17:59:39 +0800

    added simulationPage

---
+++ src/Controller/Navigation.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public abstract class Navigation {
+    public void loadStage(String fxml, ActionEvent actionEvent){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource(fxml));
+            Stage stage = new Stage();
+            stage.setScene((new Scene(root)));
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void closeCurScene(ActionEvent actionEvent){
+        Node node = (Node)(actionEvent.getSource());
+        node.getScene().getWindow().hide();
+    }
+}
